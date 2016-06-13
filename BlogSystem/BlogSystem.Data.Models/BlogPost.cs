1156f48e4fbe2e5941c04a574ce01929904ea83d
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-06-13 08:37:22 +0300

    Added BlogPost and PostComment model

---
+++ BlogSystem/BlogSystem.Data.Models/BlogPost.cs
@@ -0,0 +1,35 @@
+﻿namespace BlogSystem.Data.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    public class BlogPost
+    {
+        public BlogPost()
+        {
+            this.Date = DateTime.Now;
+            this.Comments = new HashSet<PostComment>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        [MinLength(10, ErrorMessage = "The {0} must be at least {1} characters long.")]
+        public string Content { get; set; }
+
+        [Required]
+        public string AuthorId { get; set; }
+
+        public virtual ApplicationUser Author { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; }
+
+        public virtual ICollection<PostComment> Comments { get; set; }
+    }
+}
---
+++ BlogSystem/BlogSystem.Data.Models/PostComment.cs
@@ -0,0 +1,28 @@
+﻿namespace BlogSystem.Data.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public class PostComment
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(10, ErrorMessage = "The {0} must be at least {1} characters long.")]
+        public string Content { get; set; }
+
+        [Required]
+        public int BlogPostId { get; set; }
+
+        public virtual BlogPost BlogPost { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+
+        public virtual ApplicationUser User { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; }
+    }
+}
