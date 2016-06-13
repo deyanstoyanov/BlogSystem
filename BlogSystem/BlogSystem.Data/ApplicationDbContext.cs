77febc82431acba3b19407a54b00cd210a256948
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-06-13 08:22:10 +0300

    ApplicationDbContext class moved to BlogSystem.Data

---
+++ BlogSystem/BlogSystem.Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+namespace BlogSystem.Data
+{
+    using BlogSystem.Data.Models;
+
+    using Microsoft.AspNet.Identity.EntityFramework;
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
