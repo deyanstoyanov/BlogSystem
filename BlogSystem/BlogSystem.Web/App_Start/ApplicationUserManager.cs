cec2fb3edb1a4b2d5af13afd7cea17bbcddd42bd
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-06-14 19:38:56 +0300

    Extracted classes from IdentityConfig.cs to separe .cs files

--- BlogSystem/BlogSystem.Web/App_Start/ApplicationUserManager.cs
+++ BlogSystem/BlogSystem.Web/App_Start/ApplicationUserManager.cs
@@ -1,8 +1,6 @@
-﻿namespace BlogSystem.Web
+namespace BlogSystem.Web
 {
     using System;
-    using System.Security.Claims;
-    using System.Threading.Tasks;
 
     using BlogSystem.Data;
     using BlogSystem.Data.Models;
@@ -11,27 +9,7 @@
     using Microsoft.AspNet.Identity.EntityFramework;
     using Microsoft.AspNet.Identity.Owin;
     using Microsoft.Owin;
-    using Microsoft.Owin.Security;
 
-    public class EmailService : IIdentityMessageService
-    {
-        public Task SendAsync(IdentityMessage message)
-        {
-            // Plug in your email service here to send an email.
-            return Task.FromResult(0);
-        }
-    }
-
-    public class SmsService : IIdentityMessageService
-    {
-        public Task SendAsync(IdentityMessage message)
-        {
-            // Plug in your SMS service here to send a text message.
-            return Task.FromResult(0);
-        }
-    }
-
-    // Configure the application user manager used in this application. UserManager is defined in ASP.NET Identity and is used by the application.
     public class ApplicationUserManager : UserManager<ApplicationUser>
     {
         public ApplicationUserManager(IUserStore<ApplicationUser> store)
@@ -90,31 +68,6 @@
             }
 
             return manager;
-        }
-    }
-
-    // Configure the application sign-in manager which is used in this application.
-    public class ApplicationSignInManager : SignInManager<ApplicationUser, string>
-    {
-        public ApplicationSignInManager(
-            ApplicationUserManager userManager, 
-            IAuthenticationManager authenticationManager)
-            : base(userManager, authenticationManager)
-        {
-        }
-
-        public override Task<ClaimsIdentity> CreateUserIdentityAsync(ApplicationUser user)
-        {
-            return user.GenerateUserIdentityAsync((ApplicationUserManager)this.UserManager);
-        }
-
-        public static ApplicationSignInManager Create(
-            IdentityFactoryOptions<ApplicationSignInManager> options, 
-            IOwinContext context)
-        {
-            return new ApplicationSignInManager(
-                context.GetUserManager<ApplicationUserManager>(), 
-                context.Authentication);
         }
     }
 }
