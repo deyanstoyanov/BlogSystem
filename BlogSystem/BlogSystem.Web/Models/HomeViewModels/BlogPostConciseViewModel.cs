cb5c3a5b1a90c34897eef02ed9de1b9352efb300
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-06-15 14:00:19 +0300

    Fixed problem with AutoMapper configuration

--- BlogSystem/BlogSystem.Web/Models/HomeViewModels/BlogPostConciseViewModel.cs
+++ BlogSystem/BlogSystem.Web/Models/HomeViewModels/BlogPostConciseViewModel.cs
@@ -2,10 +2,12 @@
 {
     using System;
 
+    using AutoMapper;
+
     using BlogSystem.Data.Models;
     using BlogSystem.Web.Infrastructure.Mapping;
 
-    public class BlogPostConciseViewModel : IMapFrom<BlogPost>
+    public class BlogPostConciseViewModel : IMapFrom<BlogPost>, IHaveCustomMappings
     {
         public int Id { get; set; }
 
@@ -14,5 +16,11 @@
         public string Author { get; set; }
 
         public DateTime CreatedOn { get; set; }
+
+        public void CreateMappings(IMapperConfiguration configuration)
+        {
+            configuration.CreateMap<BlogPost, BlogPostConciseViewModel>()
+                .ForMember(model => model.Author, config => config.MapFrom(post => post.Author.UserName));
+        }
     }
 }
