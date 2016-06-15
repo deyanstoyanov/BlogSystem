d8f212bf9c24ce065819a663e9a86341f8bfc1b9
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-06-15 20:25:51 +0300

    Created BlogPostViewModel. Next step - create BlogContoller.

---
+++ BlogSystem/BlogSystem.Web/Models/BlogViewModels/BlogPostViewModel.cs
@@ -0,0 +1,28 @@
+﻿namespace BlogSystem.Web.Models.BlogViewModels
+{
+    using System;
+
+    using AutoMapper;
+
+    using BlogSystem.Data.Models;
+    using BlogSystem.Web.Infrastructure.Mapping;
+
+    public class BlogPostViewModel : IMapFrom<BlogPost>, IHaveCustomMappings
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public string Author { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public void CreateMappings(IMapperConfiguration configuration)
+        {
+            configuration.CreateMap<BlogPost, BlogPostViewModel>()
+                .ForMember(model => model.Author, config => config.MapFrom(post => post.Author.UserName));
+        }
+    }
+}
