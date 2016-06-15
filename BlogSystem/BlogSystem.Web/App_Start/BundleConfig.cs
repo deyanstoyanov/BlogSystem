99a0b18ce64077731fe87e58c079cc5071078d6d
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-06-15 15:07:57 +0300

    Added site.blog.css for custom styles

--- BlogSystem/BlogSystem.Web/App_Start/BundleConfig.cs
+++ BlogSystem/BlogSystem.Web/App_Start/BundleConfig.cs
@@ -18,7 +18,7 @@
             bundles.Add(
                 new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.js", "~/Scripts/respond.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/bootstrap.css", "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/bootstrap.css", "~/Content/site.css", "~/Content/site.blog.css"));
         }
     }
 }
