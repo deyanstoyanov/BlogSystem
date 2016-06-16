5fc107ce10b09989468336c20335410dd61499ce
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-06-16 09:19:49 +0300

    Created BlogPostsController and index page with all posts

---
+++ BlogSystem/BlogSystem.Web/Areas/Administration/Controllers/BlogPostsController.cs
@@ -0,0 +1,23 @@
+﻿namespace BlogSystem.Web.Areas.Administration.Controllers
+{
+    using System.Data.Entity;
+    using System.Linq;
+    using System.Web.Mvc;
+
+    using BlogSystem.Data.UnitOfWork;
+
+    public class BlogPostsController : AdministrationController
+    {
+        public BlogPostsController(IBlogSystemData data)
+            : base(data)
+        {
+        }
+
+        // GET: Administration/BlogPosts
+        public ActionResult Index()
+        {
+            var blogPosts = this.Data.Posts.All().Include(b => b.Author);
+            return this.View(blogPosts.ToList());
+        }
+    }
+}
