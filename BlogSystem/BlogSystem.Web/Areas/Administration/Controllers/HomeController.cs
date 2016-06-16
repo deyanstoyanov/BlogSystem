6699fe869890a77e5e56b5f6c66aadf4f90a7bb6
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-06-16 09:08:03 +0300

    Created HomeController for Administration area

---
+++ BlogSystem/BlogSystem.Web/Areas/Administration/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿namespace BlogSystem.Web.Areas.Administration.Controllers
+{
+    using System.Web.Mvc;
+
+    using BlogSystem.Data.UnitOfWork;
+
+    public class HomeController : AdministrationController
+    {
+        public HomeController(IBlogSystemData data)
+            : base(data)
+        {
+        }
+
+        // GET: Administration/Home
+        public ActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
