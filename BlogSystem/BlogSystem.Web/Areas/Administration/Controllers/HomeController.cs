2f9f091da70a406c09984985a3b96b24f06e1d7d
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-06-19 21:35:22 +0300

    Implemented displaying count of blog posts at administration index page

--- BlogSystem/BlogSystem.Web/Areas/Administration/Controllers/HomeController.cs
+++ BlogSystem/BlogSystem.Web/Areas/Administration/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 ﻿namespace BlogSystem.Web.Areas.Administration.Controllers
 {
+    using System.Linq;
     using System.Web.Mvc;
 
     using BlogSystem.Data.UnitOfWork;
+    using BlogSystem.Web.Areas.Administration.Models.HomeViewModels;
 
     public class HomeController : AdministrationController
     {
@@ -14,7 +16,10 @@
         // GET: Administration/Home
         public ActionResult Index()
         {
-            return this.View();
+            var blogPosts = this.Data.Posts.All().Count();
+            var model = new IndexAdminPageViewModel { BlogPostsCount = blogPosts };
+
+            return this.View(model);
         }
     }
 }
