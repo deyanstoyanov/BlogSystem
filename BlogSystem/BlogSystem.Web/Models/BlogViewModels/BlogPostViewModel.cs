fff23edbeb385666b45a4b69df7e043b5dd63d1d
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-06-21 12:49:37 +0300

    Implemented displaying comments of post

--- BlogSystem/BlogSystem.Web/Models/BlogViewModels/BlogPostViewModel.cs
+++ BlogSystem/BlogSystem.Web/Models/BlogViewModels/BlogPostViewModel.cs
@@ -1,11 +1,13 @@
 ﻿namespace BlogSystem.Web.Models.BlogViewModels
 {
     using System;
+    using System.Collections.Generic;
 
     using AutoMapper;
 
     using BlogSystem.Data.Models;
     using BlogSystem.Web.Infrastructure.Mapping;
+    using BlogSystem.Web.Models.CommentViewModels;
 
     public class BlogPostViewModel : IMapFrom<BlogPost>, IHaveCustomMappings
     {
@@ -19,10 +21,14 @@
 
         public DateTime CreatedOn { get; set; }
 
+        public IEnumerable<CommentViewModel> Comments { get; set; }
+
         public void CreateMappings(IMapperConfiguration configuration)
         {
             configuration.CreateMap<BlogPost, BlogPostViewModel>()
                 .ForMember(model => model.Author, config => config.MapFrom(post => post.Author.UserName));
+            configuration.CreateMap<BlogPost, BlogPostViewModel>()
+                .ForMember(model => model.Comments, config => config.MapFrom(post => post.Comments));
         }
     }
 }
