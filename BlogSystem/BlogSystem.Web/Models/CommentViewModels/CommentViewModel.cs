b9dbb929ec3f2fc71be0a9314104aa008308e7d7
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-06-21 12:46:12 +0300

    Created CommentViewModel

---
+++ BlogSystem/BlogSystem.Web/Models/CommentViewModels/CommentViewModel.cs
@@ -0,0 +1,30 @@
+﻿namespace BlogSystem.Web.Models.CommentViewModels
+{
+    using System;
+
+    using AutoMapper;
+
+    using BlogSystem.Data.Models;
+    using BlogSystem.Web.Infrastructure.Mapping;
+
+    public class CommentViewModel : IMapFrom<PostComment>, IHaveCustomMappings
+    {
+        public int Id { get; set; }
+
+        public string Content { get; set; }
+
+        public int BlogPostId { get; set; }
+
+        public string User { get; set; }
+
+        public DateTime CommentedOn { get; set; }
+
+        public void CreateMappings(IMapperConfiguration configuration)
+        {
+            configuration.CreateMap<PostComment, CommentViewModel>()
+                .ForMember(model => model.CommentedOn, config => config.MapFrom(post => post.CreatedOn));
+            configuration.CreateMap<PostComment, CommentViewModel>()
+                .ForMember(model => model.User, config => config.MapFrom(post => post.User.UserName));
+        }
+    }
+}
