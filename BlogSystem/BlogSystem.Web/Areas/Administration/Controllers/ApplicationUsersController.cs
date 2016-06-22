2b8a01a015cb4cd92f2725eb7592d7f642d3b26e
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-06-22 13:09:11 +0300

    Created ApplicationUsersController and index page with all users

---
+++ BlogSystem/BlogSystem.Web/Areas/Administration/Controllers/ApplicationUsersController.cs
@@ -0,0 +1,26 @@
+﻿namespace BlogSystem.Web.Areas.Administration.Controllers
+{
+    using System.Linq;
+    using System.Web.Mvc;
+
+    using AutoMapper.QueryableExtensions;
+
+    using BlogSystem.Data.UnitOfWork;
+    using BlogSystem.Web.Areas.Administration.Models.ApplicationUsersViewModels;
+
+    public class ApplicationUsersController : AdministrationController
+    {
+        public ApplicationUsersController(IBlogSystemData data)
+            : base(data)
+        {
+        }
+
+        // GET: Administration/ApplicationUsers
+        public ActionResult Index()
+        {
+            var model = this.Data.Users.All().ProjectTo<ApplicationUserViewModel>().ToList();
+
+            return this.View(model);
+        }
+    }
+}
