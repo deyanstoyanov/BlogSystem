59e21463541f676bb0f6b02e5ec96f15809ce527
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-06-23 09:55:08 +0300

    Implemented editing user account by administrator

--- BlogSystem/BlogSystem.Web/Areas/Administration/Controllers/ApplicationUsersController.cs
+++ BlogSystem/BlogSystem.Web/Areas/Administration/Controllers/ApplicationUsersController.cs
@@ -1,5 +1,6 @@
 ﻿namespace BlogSystem.Web.Areas.Administration.Controllers
 {
+    using System;
     using System.Linq;
     using System.Net;
     using System.Web.Mvc;
@@ -7,7 +8,11 @@
     using AutoMapper.QueryableExtensions;
 
     using BlogSystem.Data.UnitOfWork;
+    using BlogSystem.Web.Areas.Administration.Models.ApplicationUsersInputModels;
     using BlogSystem.Web.Areas.Administration.Models.ApplicationUsersViewModels;
+
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
 
     public class ApplicationUsersController : AdministrationController
     {
@@ -36,6 +41,77 @@
             if (applicationUser == null)
             {
                 return this.HttpNotFound();
+            }
+
+            return this.View(applicationUser);
+        }
+
+        // GET: Administration/ApplicationUsers/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var applicationUser = this.Data.Users.Find(id);
+            if (applicationUser == null)
+            {
+                return this.HttpNotFound();
+            }
+
+            var model = new ApplicationUserEditModel
+                            {
+                                Id = applicationUser.Id, 
+                                Email = applicationUser.Email, 
+                                UserName = applicationUser.UserName, 
+                                PasswordHash = applicationUser.PasswordHash, 
+                                SecurityStamp = applicationUser.SecurityStamp
+                            };
+
+            return this.View(model);
+        }
+
+        // POST: Administration/ApplicationUsers/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(ApplicationUserEditModel applicationUser)
+        {
+            if (this.ModelState.IsValid)
+            {
+                var user = this.Data.Users.Find(applicationUser.Id);
+                var checkEmail = this.Data.Users.All().Any(e => e.Email == applicationUser.Email);
+                var checkUsername = this.Data.Users.All().Any(u => u.UserName == applicationUser.UserName);
+
+                if (checkEmail && user.Email != applicationUser.Email)
+                {
+                    return this.Content($"Email {applicationUser.Email} is already taken.");
+                }
+
+                if (checkUsername && user.UserName != applicationUser.UserName)
+                {
+                    return this.Content($"Username {applicationUser.UserName} is already taken.");
+                }
+
+                user.Id = applicationUser.Id;
+                user.Email = applicationUser.Email;
+                user.UserName = applicationUser.UserName;
+
+                if (user.PasswordHash != applicationUser.PasswordHash)
+                {
+                    var userManager = new UserManager<IdentityUser>(new UserStore<IdentityUser>());
+
+                    // userManager.RemovePassword(applicationUser.Id);       
+                    // userManager.AddPassword(applicationUser.Id, applicationUser.PasswordHash);
+                    var newPassword = userManager.PasswordHasher.HashPassword(applicationUser.PasswordHash);
+                    user.PasswordHash = newPassword;
+                    user.SecurityStamp = Guid.NewGuid().ToString();
+                }
+
+                this.Data.Users.Update(user);
+                this.Data.SaveChanges();
+
+                return this.RedirectToAction("Index");
             }
 
             return this.View(applicationUser);
