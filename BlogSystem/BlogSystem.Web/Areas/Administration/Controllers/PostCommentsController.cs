c7085af277a60491f184f1cd083ccce5711d5b80
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-06-24 21:03:44 +0300

    Created PostCommentsController and index page with all comments

---
+++ BlogSystem/BlogSystem.Web/Areas/Administration/Controllers/PostCommentsController.cs
@@ -0,0 +1,24 @@
+﻿namespace BlogSystem.Web.Areas.Administration.Controllers
+{
+    using System.Data.Entity;
+    using System.Linq;
+    using System.Web.Mvc;
+
+    using BlogSystem.Data.UnitOfWork;
+
+    public class PostCommentsController : AdministrationController
+    {
+        public PostCommentsController(IBlogSystemData data)
+            : base(data)
+        {
+        }
+
+        // GET: Administration/PostComments
+        public ActionResult Index()
+        {
+            var postComments = this.Data.PostComments.All().Include(p => p.BlogPost).Include(p => p.User).ToList();
+
+            return this.View(postComments);
+        }
+    }
+}
