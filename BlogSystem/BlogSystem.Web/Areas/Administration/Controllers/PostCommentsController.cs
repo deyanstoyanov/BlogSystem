a584a62da89b540185d8f4512deaf897dae04987
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-06-26 11:40:32 +0300

    Implemented comment details page

--- BlogSystem/BlogSystem.Web/Areas/Administration/Controllers/PostCommentsController.cs
+++ BlogSystem/BlogSystem.Web/Areas/Administration/Controllers/PostCommentsController.cs
@@ -2,6 +2,7 @@
 {
     using System.Data.Entity;
     using System.Linq;
+    using System.Net;
     using System.Web.Mvc;
 
     using BlogSystem.Data.UnitOfWork;
@@ -19,6 +20,23 @@
             var postComments = this.Data.PostComments.All().Include(p => p.BlogPost).Include(p => p.User).ToList();
 
             return this.View(postComments);
+        }
+
+        // GET: Administration/PostComments/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var postComment = this.Data.PostComments.Find(id);
+            if (postComment == null)
+            {
+                return this.HttpNotFound();
+            }
+
+            return this.View(postComment);
         }
     }
 }
