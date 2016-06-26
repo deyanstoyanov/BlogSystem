3c2d2b89842908de8c515bf0197661d6127318f3
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-06-26 15:00:52 +0300

    Implemented editing comment by administrator

--- BlogSystem/BlogSystem.Web/Areas/Administration/Controllers/PostCommentsController.cs
+++ BlogSystem/BlogSystem.Web/Areas/Administration/Controllers/PostCommentsController.cs
@@ -5,6 +5,7 @@
     using System.Net;
     using System.Web.Mvc;
 
+    using BlogSystem.Data.Models;
     using BlogSystem.Data.UnitOfWork;
 
     public class PostCommentsController : AdministrationController
@@ -35,6 +36,47 @@
             {
                 return this.HttpNotFound();
             }
+
+            return this.View(postComment);
+        }
+
+        // GET: Administration/PostComments/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var postComment = this.Data.PostComments.Find(id);
+            if (postComment == null)
+            {
+                return this.HttpNotFound();
+            }
+
+            this.ViewBag.BlogPostId = new SelectList(this.Data.Posts.All(), "Id", "Title", postComment.BlogPostId);
+            this.ViewBag.UserId = new SelectList(this.Data.Users.All(), "Id", "Email", postComment.UserId);
+
+            return this.View(postComment);
+        }
+
+        // POST: Administration/PostComments/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(
+            [Bind(Include = "Id,Content,BlogPostId,UserId,IsDeleted,DeletedOn,CreatedOn,ModifiedOn")] PostComment
+                postComment)
+        {
+            if (this.ModelState.IsValid)
+            {
+                this.Data.PostComments.Update(postComment);
+                this.Data.SaveChanges();
+
+                return this.RedirectToAction("Index");
+            }
+
+            this.ViewBag.BlogPostId = new SelectList(this.Data.Posts.All(), "Id", "Title", postComment.BlogPostId);
+            this.ViewBag.UserId = new SelectList(this.Data.Users.All(), "Id", "Email", postComment.UserId);
 
             return this.View(postComment);
         }
