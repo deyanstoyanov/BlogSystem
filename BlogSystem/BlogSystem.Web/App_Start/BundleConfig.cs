ac3c796de4684a52324474cb6cfdca5046832824
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-08-04 22:55:44 +0300

    Small cleanups in BundleConfig

--- BlogSystem/BlogSystem.Web/App_Start/BundleConfig.cs
+++ BlogSystem/BlogSystem.Web/App_Start/BundleConfig.cs
@@ -7,21 +7,28 @@
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Scripts/jquery-{version}.js"));
+            RegisterScripts(bundles);
+            RegisterStyles(bundles);
+        }
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include("~/Scripts/jquery.validate*"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(
-                new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.js", "~/Scripts/respond.js"));
-
+        private static void RegisterStyles(BundleCollection bundles)
+        {
             bundles.Add(
                 new StyleBundle("~/Content/css").Include(
                     "~/Content/bootstrap.css", 
                     "~/Content/site.css", 
                     "~/Content/site.bootstrap.darkly.custom.css", 
                     "~/Content/site.blog.css"));
+        }
+
+        private static void RegisterScripts(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include("~/Scripts/jquery.validate*"));
+
+            bundles.Add(
+                new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.js", "~/Scripts/respond.js"));
         }
     }
 }
